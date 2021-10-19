a37a2655e0c7f565089882ec2837801ae1a7c5e3
Dmitry Karkanitsa <aakarkanica@gmail.com>
2021-10-20 00:49:53 +0300

    add neccessary folders, add custom exceptions

---
+++ src/Business/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Business.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
