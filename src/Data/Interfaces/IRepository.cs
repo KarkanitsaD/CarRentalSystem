fbdac20227b7d8e1e135a31c4766c748bd214e7c
ICX\dzmitry.karkanitsa <dzmitry.karkanitsa@itechart-group.com>
2021-10-20 16:43:38 +0300

    delete superfluous methods from repository

--- src/Data/Interfaces/IRepository.cs
+++ src/Data/Interfaces/IRepository.cs
@@ -6,10 +6,8 @@ namespace Data.Interfaces
 {
     public interface IRepository<TEntity, TKey> where TEntity : class, IEntity<TKey>
     {
-        Task<int> Count(FilterRule<TEntity, TKey> filterRule = null);
-        Task<TEntity> GetAsync(FilterRule<TEntity, TKey> filterRule = null);
-        Task<IList<TEntity>> GetListAsync(QueryParameters<TEntity, TKey> queryParameters = null);
-        Task<IList<TEntity>> GetPageListAsync(QueryParameters<TEntity, TKey> queryParameters = null);
+        Task<TEntity> GetAsync(TKey id);
+        Task<IList<TEntity>> GetListAsync();
         Task<TEntity> CreateAsync(TEntity entity);
         TEntity Update(TEntity entity);
         Task<TEntity> DeleteAsync(TKey id);
--- src/Data/Repositories/Repository.cs
+++ src/Data/Repositories/Repository.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Data.Interfaces;
-using Data.Query;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repositories
@@ -18,115 +15,38 @@ namespace Data.Repositories
             Set = context.Set<TEntity>();
         }
 
-        public virtual async Task<int> Count(FilterRule<TEntity, TKey> filterRule = null)
+        public async Task<TEntity> GetAsync(TKey id)
         {
-            var query = Set.AsQueryable();
-
-            if (filterRule == null)
-                return await query.CountAsync();
-
-            return await FilterQuery(query, filterRule).CountAsync();
+            return await Set.AsQueryable().FirstOrDefaultAsync(entity => entity.Id.Equals(id));
         }
 
-        public virtual async Task<TEntity> GetAsync(FilterRule<TEntity, TKey> filterRule = null)
+        public async Task<IList<TEntity>> GetListAsync()
         {
-            var query = Set.AsQueryable();
-
-            if (filterRule == null)
-                return await query.FirstOrDefaultAsync();
-
-            return await FilterQuery(query, filterRule).FirstOrDefaultAsync();
+            return await Set.AsQueryable().ToListAsync();
         }
 
-        public virtual async Task<IList<TEntity>> GetListAsync(QueryParameters<TEntity, TKey> queryParameters = null)
+        public async Task<TEntity> CreateAsync(TEntity entity)
         {
-            var query = Set.AsQueryable();
+            var createdEntity = await Set.AddAsync(entity);
 
-            if (queryParameters == null)
-                return await query.ToListAsync();
-
-            query = BaseQuery(query, queryParameters);
-
-            return await query.ToListAsync();
+            return createdEntity.Entity;
         }
 
-        public virtual async Task<IList<TEntity>> GetPageListAsync(QueryParameters<TEntity, TKey> queryParameters = null)
+        public TEntity Update(TEntity entity)
         {
-            var query = Set.AsQueryable();
+            var updatedEntity = Set.Update(entity);
 
-            if (queryParameters == null)
-                return await query.ToListAsync();
-
-            query = BaseQuery(query, queryParameters);
-
-            if (queryParameters.PaginationRule != null)
-                query = PaginationQuery(query, queryParameters.PaginationRule);
-
-            return await query.ToListAsync();
+            return updatedEntity.Entity;
         }
 
-        public virtual async Task<TEntity> CreateAsync(TEntity entity)
-        {
-            var result = await Set.AddAsync(entity);
-            return result.Entity;
-        }
-
-        public virtual TEntity Update(TEntity entity)
-        {
-            var result = Set.Update(entity);
-            return result.Entity;
-        }
-
-        public virtual async Task<TEntity> DeleteAsync(TKey id)
+        public async Task<TEntity> DeleteAsync(TKey id)
         {
             var entityToDelete = await Set.FindAsync(id);
 
             if (entityToDelete != null)
-            {
-                Set.Remove(entityToDelete);
-            }
-
-            return entityToDelete;
-        }
-
-        protected virtual IQueryable<TEntity> BaseQuery(IQueryable<TEntity> queryable, QueryParameters<TEntity, TKey> queryParameters)
-        {
-            if (queryParameters.FilterRule?.FilterExpression != null)
-                queryable = FilterQuery(queryable, queryParameters.FilterRule);
-
-            if (queryParameters.SortRule?.SortExpression != null)
-                queryable = SortQuery(queryable, queryParameters.SortRule);
-
-            return queryable;
-        }
-
-        protected virtual IQueryable<TEntity> FilterQuery(IQueryable<TEntity> queryable, FilterRule<TEntity, TKey> filterRule)
-        {
-            queryable = queryable.Where(filterRule.FilterExpression);
-
-            return queryable;
-        }
-
-        protected virtual IQueryable<TEntity> SortQuery(IQueryable<TEntity> queryable, SortRule<TEntity, TKey> sortRule)
-        {
-            queryable = sortRule.InAscendingOrder
-                    ? queryable.OrderBy(sortRule.SortExpression)
-                    : queryable.OrderByDescending(sortRule.SortExpression);
-
-            return queryable;
-        }
-
-        protected virtual IQueryable<TEntity> PaginationQuery(IQueryable<TEntity> queryable, PaginationRule paginationRule)
-        {
-            if (paginationRule == null)
-                throw new ArgumentException("Pagination rule can't be null.", nameof(paginationRule));
-
-            if (paginationRule.Index < 0 || paginationRule.Size <= 0)
-                throw new ArgumentException("Pagination rule is not valid.", nameof(paginationRule));
-
-            queryable = queryable.Skip(paginationRule.Index * paginationRule.Size).Take(paginationRule.Size);
-
-            return queryable;
+                return Set.Remove(entityToDelete).Entity;
+            
+            return null;
         }
     }
 }
