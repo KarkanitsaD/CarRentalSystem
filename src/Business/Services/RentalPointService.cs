80f6554f0de4cccac562b3e817196f73b7ecf9ab
Dmitry Karkanitsa <aakarkanica@gmail.com>
2021-10-20 09:35:08 +0300

    implement rental point service

--- src/Business/Services/RentalPointService.cs
+++ src/Business/Services/RentalPointService.cs
@@ -1,56 +1,145 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
+using Business.Exceptions;
 using Business.FilterModels;
 using Business.Interfaces;
 using Business.Models;
+using Data;
+using Data.Models;
+using Data.Query;
+using Data.Repositories;
 
 namespace Business.Services
 {
     public class RentalPointService : IRentalPointService
     {
-        public Task<RentalPointModel> GetAsync(int id)
+        private readonly IMapper _mapper;
+        private readonly ApplicationContext _context;
+        private readonly RentalPointRepository _rentalPointRepository;
+
+        public RentalPointService(IMapper mapper, ApplicationContext context, RentalPointRepository rentalPointRepository)
         {
-            throw new System.NotImplementedException();
+            _mapper = mapper;
+            _context = context;
+            _rentalPointRepository = rentalPointRepository;
         }
 
-        public Task<IList<RentalPointModel>> GetListAsync(RentalPointFilterModel rentalPointFilterModel)
+        public async Task<RentalPointModel> GetAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var filterRule = new FilterRule<RentalPointEntity, int>
+            {
+                FilterExpression = rentalPoint =>
+                    rentalPoint.Id == id
+            };
+
+            var entity = await _rentalPointRepository.GetAsync(filterRule);
+
+            return _mapper.Map<RentalPointEntity, RentalPointModel>(entity);
         }
 
-        public Task<IList<RentalPointModel>> GetPageListAsync(RentalPointFilterModel rentalPointFilterModel)
+        public async Task<IList<RentalPointModel>> GetListAsync(RentalPointFilterModel rentalPointFilterModel)
         {
-            throw new System.NotImplementedException();
+            var filterRule = DefineFilterRule(rentalPointFilterModel);
+            var sortRule = DefineSortRule(rentalPointFilterModel);
+            var queryParameters = new QueryParameters<RentalPointEntity, int>()
+            {
+                FilterRule = filterRule,
+                SortRule = sortRule
+            };
+
+            var entities = await _rentalPointRepository.GetListAsync(queryParameters);
+
+            return _mapper.Map<IList<RentalPointEntity>, IList<RentalPointModel>>(entities);
         }
 
-        public Task CreateAsync(RentalPointModel rentalPointModel)
+        public async Task<IList<RentalPointModel>> GetPageListAsync(RentalPointFilterModel rentalPointFilterModel)
         {
-            throw new System.NotImplementedException();
+            var filterRule = DefineFilterRule(rentalPointFilterModel);
+            var sortRule = DefineSortRule(rentalPointFilterModel);
+            var queryParameters = new QueryParameters<RentalPointEntity, int>()
+            {
+                FilterRule = filterRule,
+                SortRule = sortRule,
+                PaginationRule = new PaginationRule()
+                {
+                    Index = rentalPointFilterModel.PageIndex,
+                    Size = rentalPointFilterModel.PageSize
+                }
+            };
+
+            var entities = await _rentalPointRepository.GetPageListAsync(queryParameters);
+
+            return _mapper.Map<IList<RentalPointEntity>, IList<RentalPointModel>>(entities);
         }
 
-        public Task UpdateAsync(RentalPointModel rentalPointModel)
+        public async Task CreateAsync(RentalPointModel rentalPointModel)
         {
-            throw new System.NotImplementedException();
+            var entity = _mapper.Map<RentalPointModel, RentalPointEntity>(rentalPointModel);
+
+            await _rentalPointRepository.CreateAsync(entity);
+
+            await _context.SaveChangesAsync();
         }
 
-        public Task DeleteAsync(int id)
+        public async Task UpdateAsync(RentalPointModel rentalPointModel)
         {
-            throw new System.NotImplementedException();
+            var entity = _mapper.Map<RentalPointModel, RentalPointEntity>(rentalPointModel);
+
+            _rentalPointRepository.Update(entity);
+
+            await _context.SaveChangesAsync();
         }
 
-        public Task<IList<string>> GetAllCountries()
+        public async Task DeleteAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var entityToDelete = await _rentalPointRepository.DeleteAsync(id);
+
+            if (entityToDelete == null)
+                throw new NotFoundException("Entity not found.");
+
+            await _context.SaveChangesAsync();
         }
 
-        public Task<IList<string>> GetAllCities()
+        public async Task<IList<string>> GetAllCountries()
         {
-            throw new System.NotImplementedException();
+            var countries = await _rentalPointRepository.GetRentalPointsCountriesAsync();
+
+            return countries;
         }
 
-        public Task<IList<string>> GetTitles()
+        public async Task<IList<string>> GetAllCities()
         {
-            throw new System.NotImplementedException();
+            var cities = await _rentalPointRepository.GetRentalPointsCitiesAsync();
+
+            return cities;
+        }
+
+        public async Task<IList<string>> GetTitles()
+        {
+            var titles = await _rentalPointRepository.GetRentalPointTitlesAsync();
+
+            return titles;
+        }
+
+        private FilterRule<RentalPointEntity, int> DefineFilterRule(RentalPointFilterModel rentalPointFilterModel)
+        {
+            throw new NotImplementedException();
+            //TODO: write logic to filter rental points
+        }
+
+        private SortRule<RentalPointEntity, int> DefineSortRule(RentalPointFilterModel rentalPointFilterModel)
+        {
+            var sortRule = new SortRule<RentalPointEntity, int>();
+
+            if (rentalPointFilterModel.InAscendingOrder != null)
+            {
+                sortRule.InAscendingOrder = (bool)rentalPointFilterModel.InAscendingOrder;
+                sortRule.SortExpression = rentalPoint => rentalPoint.Cars.Count;
+            }
+
+            return sortRule;
         }
     }
 }
