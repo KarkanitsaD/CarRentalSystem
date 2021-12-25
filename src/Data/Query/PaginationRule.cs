7bd6d181a0ba855e2dcb662419032b943cfdfd11
Dmitry Karkanitsa <aakarkanica@gmail.com>
2021-12-25 18:14:32 +0300

    delete unnesassary files

--- src/Data/Query/PaginationRule.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Data.Query
-{
-    public class PaginationRule
-    {
-        public int Index { get; set; } = 0;
-        public int? Size { get; set; }
-        public bool IsValid => Size != null && Index >= 0 && Size > 0;
-    }
-}
